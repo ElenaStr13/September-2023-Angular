6b78421bc6cc1012149ea97fd4233d931de10164
ElenaStr13 <estrateluk@yahoo.com>
2024-03-30 19:17:43 +0200

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {UsersComponent} from "./components/users/users.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-    title = 'sep2023Angular';
+  title = 'sep2023Angular';
 }
