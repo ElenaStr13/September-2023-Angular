0d1176792b443697adbd877b7bd680b9eacdab6b
ElenaStr13 <estrateluk@yahoo.com>
2024-04-04 18:33:00 +0300

    Lesson 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import {HttpClientModule} from "@angular/common/http";
-import {UsersModule} from "./modules/users/users.module";
-import {PostsModule} from "./modules/posts/posts.module";
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainLayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
